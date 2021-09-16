15c136c2c086c6e10e2b4f611a8770603503cc16
Alvaro Molina <alvmol706@alu.centroafuera.es>
2021-09-16 13:06:59 +0200

    completar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main (){
     println("Mi primer programa en Kotlin")
+
+    var a= "A"
+    val b = "B"
+
+    var c : String? = "A"
+    c=null
+    if (c != null) {
+        c.length
+    }
 }
